b37430d567313223132feabe6aa7880ac82f23aa
Alex <alexanderekdahl@me.com>
2021-12-13 14:22:50 +0100

    Removed client

--- client/src/utils/utility.js
+++
@@ -1,8 +0,0 @@
-export const createAmenityArray = (wifi, kitchen, bath, parking) => {
-  let amenityArray = []
-  if (wifi) amenityArray.push('WIFI')
-  if (kitchen) amenityArray.push('KITCHEN')
-  if (bath) amenityArray.push('BATH')
-  if (parking) amenityArray.push('PARKING')
-  return amenityArray
-}
